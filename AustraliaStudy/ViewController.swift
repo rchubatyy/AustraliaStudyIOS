c954989358d65c29a9cc07fc7b3916690a1995de
Roman Chubatyy <rchubatyy@gmail.com>
2019-11-18 12:11:59 +1100

    Initial Commit

---
+++ AustraliaStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AustraliaStudy
+//
+//  Created by user157559 on 11/18/19.
+//  Copyright © 2019 Australia Study. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
